eba1a67d09e61a354a9510424690e28071908182
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-18 12:48:50 +0200

    base install

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+)
