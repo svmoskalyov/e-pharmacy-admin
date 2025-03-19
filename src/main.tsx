8f02951e8c20cd4be197f47b6d551ce88fbf9cc1
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-19 15:00:53 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { BrowserRouter } from 'react-router'
+import { ThemeProvider } from '@mui/material'
+import { CssBaseline } from '@mui/material'
+import theme from './theme.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
 )
