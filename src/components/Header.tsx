9a257aef6a34d0d0574334e00c0d92f6f07bb7a5
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-24 15:08:32 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,40 @@
-function Header() {
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import Box from '@mui/material/Box'
+import { Icon } from '@iconify/react'
+
+interface HeaderProps {
+  onMenuClick: () => void;
+}
+
+function Header({ onMenuClick }: HeaderProps) {
   return (
-    <div>header</div>
+    <Box sx={{ display: 'flex' }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            // size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            // sx={{ mr: 2 }}
+            // sx={{ mr: 2, display: { sm: 'none' } }}
+            onClick={onMenuClick}
+          >
+            <Icon icon="majesticons:menu" width="24" height="24" />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            // sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            AppBar
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
 
