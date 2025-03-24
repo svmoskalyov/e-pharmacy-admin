f0f923c28b0911ded392b6425117638e8dc1de6d
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-24 15:08:38 +0200

    Update SharedLayout.tsx

--- src/layouts/SharedLayout.tsx
+++ src/layouts/SharedLayout.tsx
@@ -1,16 +1,48 @@
+import { useState } from 'react'
 import { Outlet } from 'react-router'
+import { Box } from '@mui/material'
+// import { useMediaQuery, useTheme } from '@mui/material'
 import Header from '../components/Header.tsx'
 import Sidebar from '../components/Sidebar.tsx'
 
 function SharedLayout() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+  // const theme = useTheme()
+  // const isDesktop = useMediaQuery(theme.breakpoints.up('desktop'))
+
+  const handleSidebarToggle = () => {
+    setIsSidebarOpen((prevState) => !prevState)
+  }
+
+  // const handleMenuClick = () => {
+  //   setIsSidebarOpen(true)
+  // }
+  //
+  // const handleSidebarClose = () => {
+  //   setIsSidebarOpen(false)
+  // }
+
   return (
-    <div>
-      <Header />
-      <div>
+    <Box>
+      <Header onMenuClick={handleSidebarToggle} />
+      <Sidebar
+        open={isSidebarOpen}
+        onClose={handleSidebarToggle}
+        // variant={isDesktop ? 'permanent' : 'temporary'}
+      />
+      <Box
+        component="main"
+        // sx={{
+        //   py: 4,
+        //   display: 'flex',
+        //   flexDirection: 'column',
+        //   alignItems: 'center',
+        //   textAlign: 'center'
+        // }}
+      >
         <Outlet />
-      </div>
-      <Sidebar />
-    </div>
+      </Box>
+    </Box>
   )
 }
 
