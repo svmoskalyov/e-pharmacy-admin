2c0d1b7efe3167b9bd5e2e00cc603cd63d408c7c
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-27 15:30:17 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,83 @@
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
-import Box from '@mui/material/Box'
+import {
+  useMediaQuery, useTheme,
+  Box, AppBar, Toolbar, Typography, IconButton, Divider
+} from '@mui/material'
 import { Icon } from '@iconify/react'
+import ButtonLogout from './ui/ButtonLogout.tsx'
+import Logo from './ui/Logo.tsx'
 
 interface HeaderProps {
   onMenuClick: () => void;
 }
 
 function Header({ onMenuClick }: HeaderProps) {
+  const theme = useTheme()
+  const isDesktop = useMediaQuery(theme.breakpoints.up('desktop'))
+
   return (
-    <Box sx={{ display: 'flex' }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            // size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            // sx={{ mr: 2 }}
-            // sx={{ mr: 2, display: { sm: 'none' } }}
-            onClick={onMenuClick}
-          >
-            <Icon icon="majesticons:menu" width="24" height="24" />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            // sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            AppBar
-          </Typography>
+    <Box sx={{ gridArea: 'header' }}>
+      <AppBar
+        position="static"
+        sx={{
+          background: 'inherit',
+          color: 'text.primary',
+          boxShadow: 0,
+          borderBottom: '1px solid rgba(29, 30, 33, 0.1)'
+        }}
+      >
+        <Toolbar
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            paddingLeft: { mobile: '16px', tablet: '32px', desktop: '20px' },
+            paddingRight: { mobile: '16px', tablet: '32px', desktop: '20px' },
+            height: '80px'
+          }}
+        >
+          {!isDesktop &&
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2, display: { desktop: 'none' } }}
+              onClick={onMenuClick}
+            >
+              <Icon icon="iconamoon:menu-burger-horizontal-duotone" width="32" height="32" />
+            </IconButton>
+          }
+          <Logo />
+          <Box sx={{ flexGrow: 1, ml: 1 }}>
+            <Typography
+              variant="h6"
+              component="div"
+              fontWeight="600"
+              lineHeight="24px"
+            >
+              Medicine store
+            </Typography>
+
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <Typography
+                variant="caption"
+                component="div"
+                lineHeight="18px"
+                color="text.secondary"
+              >
+                Dasboard
+              </Typography>
+              <Divider orientation="vertical" sx={{ height: '12px' }} />
+              <Typography
+                variant="caption"
+                component="div"
+                lineHeight="18px"
+                color="text.secondary"
+              >
+                vendor@email.com
+              </Typography>
+            </Box>
+          </Box>
+
+          {isDesktop && <ButtonLogout />}
         </Toolbar>
       </AppBar>
     </Box>
