12d9b55f2341eeaff8bf2cc3e4ac19d4d52115ce
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-30 14:21:21 +0300

    Update auth.ts

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,40 +1,10 @@
-import {
-  setPersistence,
-  browserSessionPersistence,
-  signInWithEmailAndPassword,
-  signOut
-} from 'firebase/auth'
+import { signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import { firebaseAuth } from './firebaseConfig'
 
-export async function signInWithCredentials(email: string, password: string) {
-  try {
-    return setPersistence(firebaseAuth, browserSessionPersistence).then(async () => {
-      const userCredential = await signInWithEmailAndPassword(firebaseAuth, email, password)
-      return {
-        success: true,
-        user: userCredential.user,
-        error: null
-      }
-    })
-  } catch (error: unknown) {
-    const errorMessage = error instanceof Error ? error.message : 'Failed to sign in with email/password'
-    return {
-      success: false,
-      user: null,
-      error: errorMessage
-    }
-  }
+export async function firebaseSignIn(email: string, password: string) {
+  return await signInWithEmailAndPassword(firebaseAuth, email, password)
 }
 
-export const firebaseSignOut = async () => {
-  try {
-    await signOut(firebaseAuth)
-    return { success: true }
-  } catch (error: unknown) {
-    const errorMessage = error instanceof Error ? error.message : 'An error occurred'
-    return {
-      success: false,
-      error: errorMessage
-    }
-  }
+export async function firebaseSignOut() {
+  await signOut(firebaseAuth)
 }
