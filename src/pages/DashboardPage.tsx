4482baa0f8d04169fa68d73b1faa2b6fe51b7664
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-04-01 16:47:46 +0300

    Update DashboardPage.tsx

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,6 +1,29 @@
+import { useEffect } from 'react'
+import { Box } from '@mui/material'
+import { useDataStore } from '../stores/dataStore.ts'
+import Statistics from '../components/Statistics.tsx'
+import RecentCustomers from '../components/RecentCustomers.tsx'
+import IncomeExpenses from '../components/IncomeExpenses.tsx'
+
 function DashboardPage() {
+  const getData = useDataStore((state) => state.getData)
+  const customers = useDataStore((state) => state.customers)
+  const incomeExpenses = useDataStore((state) => state.incomeExpenses)
+
+  console.log('customers --', customers)
+  console.log('incomeExpenses --', incomeExpenses)
+
+  useEffect(() => {
+    if (!customers.length) getData('customers')
+    if (!incomeExpenses.length) getData('incomeExpenses')
+  }, [customers.length, incomeExpenses.length, getData])
+
   return (
-    <div>dashboard page</div>
+    <Box>
+      <Statistics />
+      <RecentCustomers customers={customers} />
+      <IncomeExpenses incomeExpenses={incomeExpenses}/>
+    </Box>
   )
 }
 
