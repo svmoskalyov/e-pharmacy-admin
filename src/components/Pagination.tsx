7336a41264f562bc92c8617040fa30fc9b81577a
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-04-04 10:53:06 +0300

    Create Pagination.tsx

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,64 @@
+import { Box, Button } from '@mui/material'
+
+interface PaginationProps {
+  totalPages: number
+  currentPage: number
+  onPageChange: (page: number) => void
+}
+
+function Pagination({ totalPages, currentPage, onPageChange }: PaginationProps) {
+  const visiblePages = 5
+  const pageNumbers = []
+
+  let startPage = Math.max(1, currentPage - Math.floor(visiblePages / 2))
+  const endPage = Math.min(totalPages, startPage + visiblePages - 1)
+
+  if (endPage - startPage < visiblePages - 1) {
+    startPage = Math.max(1, endPage - visiblePages + 1)
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
+    pageNumbers.push(i)
+  }
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        gap: '8px',
+        marginTop: '20px'
+      }}
+    >
+      {pageNumbers.map((page) => (
+        <Button
+          key={page}
+          variant="text"
+          onClick={() => onPageChange(page)}
+          sx={{
+            justifyContent: 'flex-end',
+            alignItems: 'flex-end',
+            padding: '0',
+            minWidth: { mobile: '14px', tablet: '19px' },
+            height: { mobile: '14px', tablet: '19px' },
+            '&:hover': {
+              backgroundColor: 'transparent'
+            }
+          }}
+        >
+          <Box
+            sx={{
+              width: '12px',
+              height: '12px',
+              borderRadius: '60px',
+              backgroundColor: currentPage === page ?
+                'accent.main' : 'accent.light'
+            }}
+          ></Box>
+        </Button>
+      ))}
+    </Box>
+  )
+}
+
+export default Pagination
