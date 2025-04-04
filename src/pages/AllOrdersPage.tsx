9e0dfc060058faf45070cd3e561d0ac2a42ddb7b
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-04-04 10:53:15 +0300

    Update AllOrdersPage.tsx

--- src/pages/AllOrdersPage.tsx
+++ src/pages/AllOrdersPage.tsx
@@ -1,6 +1,45 @@
+import { useEffect, useState } from 'react'
+import { Box } from '@mui/material'
+import Filter from '../components/Filter.tsx'
+import AllOrders from '../components/AllOrders.tsx'
+import { useDataStore } from '../stores/dataStore.ts'
+import Pagination from '../components/Pagination.tsx'
+
 function AllOrdersPage() {
+  const getData = useDataStore((state) => state.getData)
+  const orders = useDataStore((state) => state.orders)
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const itemsPerPage = 5
+
+  const totalPages = Math.ceil(orders.length / itemsPerPage)
+  const startIndex = (currentPage - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  const orderItems = orders.slice(startIndex, endIndex)
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page)
+  }
+
+  useEffect(() => {
+    if (!orders.length) getData('orders')
+  }, [orders.length, getData])
+
   return (
-    <div>AllOrdersPage</div>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <Filter />
+      <AllOrders orders={orderItems} />
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </Box>
   )
 }
 
