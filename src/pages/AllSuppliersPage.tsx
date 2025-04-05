cd867ad42004dd354a6e7a27a0f1634254600d67
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-04-05 10:41:19 +0300

    Update AllSuppliersPage.tsx

--- src/pages/AllSuppliersPage.tsx
+++ src/pages/AllSuppliersPage.tsx
@@ -1,6 +1,45 @@
+import { useEffect, useState } from 'react'
+import { Box } from '@mui/material'
+import { useDataStore } from '../stores/dataStore.ts'
+import Filter from '../components/Filter.tsx'
+import Pagination from '../components/Pagination.tsx'
+import AllSuppliers from '../components/AllSuppliers.tsx'
+
 function AllSuppliersPage() {
+  const getData = useDataStore((state) => state.getData)
+  const suppliers = useDataStore((state) => state.suppliers)
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const itemsPerPage = 5
+
+  const totalPages = Math.ceil(suppliers.length / itemsPerPage)
+  const startIndex = (currentPage - 1) * itemsPerPage
+  const endIndex = startIndex + itemsPerPage
+  const supplierItems = suppliers.slice(startIndex, endIndex)
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page)
+  }
+
+  useEffect(() => {
+    if (!suppliers.length) getData('suppliers')
+  }, [suppliers.length, getData])
+
   return (
-    <div>AllSuppliersPage</div>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <Filter />
+      <AllSuppliers suppliers={supplierItems} />
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </Box>
   )
 }
 
