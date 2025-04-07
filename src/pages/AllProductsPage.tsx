756dc84642cf469e6b172b2ca17da1ed09a42335
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-04-07 09:42:11 +0300

    Update AllProductsPage.tsx

--- src/pages/AllProductsPage.tsx
+++ src/pages/AllProductsPage.tsx
@@ -37,8 +37,16 @@ function AllProductsPage() {
   }
 
   useEffect(() => {
-    if (!products.length) getData('products')
+    if (!products.length) {
+      console.log('products is empty')
+      getData('products')
+    }
+    if (products.length !== filtered.length) {
+      setFiltered(products)
+    }
   }, [products.length, getData])
+
+  console.log(products.length)
 
   return (
     <Box
