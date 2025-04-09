f27b73e7a367d7e2f197367c8a5dc4f48a6a0b7b
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-04-09 14:53:27 +0300

    Update dataStore.ts

--- src/stores/dataStore.ts
+++ src/stores/dataStore.ts
@@ -2,59 +2,56 @@ import { create } from 'zustand'
 import { createJSONStorage, persist } from 'zustand/middleware'
 import { fetchData } from '../firebase/data.ts'
 import {
-  addItemToArray,
-  removeItemByIdFromArray,
-  editItemInArray,
-  Identifiable
+  addItemToArray, removeItemByIdFromArray, editItemInArray, Identifiable
 } from '../utils/arrayUtils.ts'
 
 interface Customers extends Identifiable {
-  'id': string
-  'photo': string
-  'name': string
-  'email': string
-  'spent': string
-  'phone': string
-  'address': string
-  'register_date': string
+  id: string
+  photo: string
+  name: string
+  email: string
+  spent: string
+  phone: string
+  address: string
+  register_date: string
 }
 
 interface IncomeExpenses extends Identifiable {
-  'id': string
-  'name': string
-  'amount': string
-  'type': string
+  id: string
+  name: string
+  amount: string
+  type: string
 }
 
 interface Orders extends Identifiable {
-  'id': string
-  'photo': string
-  'name': string
-  'address': string
-  'products': string
-  'price': string
-  'status': string
-  'order_date': string
+  id: string
+  photo: string
+  name: string
+  address: string
+  products: string
+  price: string
+  status: string
+  order_date: string
 }
 
 interface Products extends Identifiable {
-  'id': string
-  'photo': string
-  'name': string
-  'suppliers': string
-  'stock': string
-  'price': string
-  'category': string
+  id: string
+  photo: string
+  name: string
+  suppliers: string
+  stock: string
+  price: string
+  category: string
 }
 
 interface Suppliers extends Identifiable {
-  'id': string
-  'name': string
-  'address': string
-  'suppliers': string
-  'date': string
-  'amount': string
-  'status': string
+  id: string
+  name: string
+  address: string
+  suppliers: string
+  date: string
+  amount: string
+  status: string
 }
 
 type ArrayName = 'customers' | 'incomeExpenses' | 'orders' |
@@ -74,10 +71,10 @@ type Actions = {
   getData: (base: string) => void
   addItem: <T extends Identifiable>(
     arrayName: ArrayName, newItem: T) => void
-  removeItemById: <T extends Identifiable>(
-    arrayName: ArrayName, idToRemove: T['id']) => void
   editItem: <T extends Identifiable>(
     arrayName: ArrayName, updatedItem: T) => void
+  removeItemById: <T extends Identifiable>(
+    arrayName: ArrayName, idToRemove: T['id']) => void
   setIsLoading: (isLoading: boolean) => void
   setError: (error: string | null) => void
 }
@@ -113,17 +110,17 @@ export const useDataStore = create<State & Actions>()(
           [arrayName]: addItemToArray<T>(
             (state[arrayName] as unknown) as T[], newItem)
         })),
-      removeItemById: <T extends Identifiable>(
-        arrayName: ArrayName, idToRemove: T['id']) =>
-        set((state) => ({
-          [arrayName]: removeItemByIdFromArray<T>(
-            (state[arrayName] as unknown) as T[], idToRemove)
-        })),
       editItem: <T extends Identifiable>(
         arrayName: ArrayName, updatedItem: T) =>
         set((state) => ({
           [arrayName]: editItemInArray<T>(
             (state[arrayName] as unknown) as T[], updatedItem)
+        })),
+      removeItemById: <T extends Identifiable>(
+        arrayName: ArrayName, idToRemove: T['id']) =>
+        set((state) => ({
+          [arrayName]: removeItemByIdFromArray<T>(
+            (state[arrayName] as unknown) as T[], idToRemove)
         })),
       setIsLoading: isLoading => set({ isLoading }),
       setError: error => set({ error })
