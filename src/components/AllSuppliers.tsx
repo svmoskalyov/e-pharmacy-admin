7ad1b2f2839c81819df98fe240a1c6a82b61a5a8
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-04-12 15:36:02 +0300

    Update AllSuppliers.tsx

--- src/components/AllSuppliers.tsx
+++ src/components/AllSuppliers.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import {
   Paper, TableRow, TableHead, TableContainer, TableBody, Table, Chip, Button
 } from '@mui/material'
@@ -5,6 +6,8 @@ import { Icon } from '@iconify/react'
 import TableName from './ui/TableName.tsx'
 import TableRowHead from './ui/TableRowHead.tsx'
 import TableRowCell from './ui/TableRowCell.tsx'
+import Popup from './ui/Popup.tsx'
+import SupplierForm from './SupplierForm.tsx'
 
 interface AllSuppliersProps {
   suppliers: {
@@ -18,123 +21,148 @@ interface AllSuppliersProps {
   }[]
 }
 
+interface Supplier {
+  id: string
+  name: string
+  address: string
+  suppliers: string
+  date: string
+  amount: string
+  status: string
+}
+
+
 function AllSuppliers({ suppliers }: AllSuppliersProps) {
+  const [openPopup, setOpenPopup] = useState<boolean>(false)
+  const [supplierItem, setSupplierItem] = useState<Supplier>()
+
+  const handleModal = () => {
+    setOpenPopup(!openPopup)
+  }
 
   const handleEdit = (id: string) => {
-    console.log('Edit product with id:', id)
+    console.log('Edit supplier with id:', id)
+    setSupplierItem(suppliers.find((item) => item.id === id))
+    handleModal()
   }
 
   return (
-    <Paper
-      sx={{
-        width: '100%',
-        maxWidth: { mobile: '335px', tablet: '704px', desktop: '1280px' },
-        borderRadius: '8px',
-        boxShadow: 'none',
-        overflow: 'hidden'
-      }}
-    >
-      <TableName>All suppliers</TableName>
-      <TableContainer
+    <>
+      <Paper
         sx={{
-          maxHeight: { mobile: '460px', tablet: '502px', desktop: '541px' }
+          width: '100%',
+          maxWidth: { mobile: '335px', tablet: '704px', desktop: '1280px' },
+          borderRadius: '8px',
+          boxShadow: 'none',
+          overflow: 'hidden'
         }}
       >
-        <Table stickyHeader aria-label="all suppliers table">
-          <TableHead>
-            <TableRow>
-              <TableRowHead sx={{ textWrap: 'nowrap' }}>
-                Suppliers Info
-              </TableRowHead>
-              <TableRowHead sx={{ textWrap: 'nowrap' }}>
-                Address
-              </TableRowHead>
-              <TableRowHead sx={{ textWrap: 'nowrap' }}>
-                Company
-              </TableRowHead>
-              <TableRowHead sx={{ textWrap: 'nowrap' }}>
-                Delivery date
-              </TableRowHead>
-              <TableRowHead sx={{ textWrap: 'nowrap' }}>
-                Ammount
-              </TableRowHead>
-              <TableRowHead sx={{ textWrap: 'nowrap' }}>
-                Status
-              </TableRowHead>
-              <TableRowHead sx={{ textWrap: 'nowrap' }}>
-                Action
-              </TableRowHead>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {suppliers.map((row, i) => (
-              <TableRow key={i}>
-                <TableRowCell sx={{ textWrap: 'nowrap' }}>
-                  {row.name}
-                </TableRowCell>
-                <TableRowCell sx={{ textWrap: 'nowrap' }}>
-                  {row.address}
-                </TableRowCell>
-                <TableRowCell sx={{ textWrap: 'nowrap' }}>
-                  {row.suppliers}
-                </TableRowCell>
-                <TableRowCell sx={{ textWrap: 'nowrap' }}>
-                  {row.date}
-                </TableRowCell>
-                <TableRowCell sx={{ textWrap: 'nowrap' }}>
-                  {row.amount}
-                </TableRowCell>
-                <TableRowCell>
-                  <Chip
-                    label={row.status}
-                    sx={{
-                      height: '25px',
-                      fontSize: { mobile: '12px', tablet: '14px' },
-                      fontWeight: 400,
-                      lineHeight: { mobile: '14px', tablet: '16px' },
-                      backgroundColor: row.status === 'Active' ?
-                        'accent.light' : 'accent2.light',
-                      color: row.status === 'Active' ?
-                        'accent.main' : 'accent2.main'
-                    }}
-                  />
-                </TableRowCell>
-                <TableRowCell>
-                  <Button
-                    sx={{
-                      gap: '4px',
-                      height: '34px',
-                      width: '82px',
-                      fontSize: { mobile: '14px', tablet: '16px' },
-                      fontWeight: 400,
-                      lineHeight: { mobile: '16px', tablet: '18px' },
-                      textTransform: 'capitalize',
-                      color: 'acton.text.edit',
-                      borderRadius: '30px',
-                      borderWidth: '1px',
-                      borderStyle: 'solid',
-                      borderColor: 'acton.bg.edit',
-                      '&:hover': {
-                        color: 'accent.dark',
-                        backgroundColor: 'accent.light'
-                      }
-                    }}
-                    onClick={() => handleEdit(row.id)}
-                  >
-                    <Icon
-                      icon="majesticons:edit-pen-2-line"
-                      width="14"
-                      height="14"
-                    />
-                    Edit
-                  </Button>
-                </TableRowCell>
+        <TableName>All suppliers</TableName>
+        <TableContainer
+          sx={{
+            maxHeight: { mobile: '460px', tablet: '502px', desktop: '541px' }
+          }}
+        >
+          <Table stickyHeader aria-label="all suppliers table">
+            <TableHead>
+              <TableRow>
+                <TableRowHead sx={{ textWrap: 'nowrap' }}>
+                  Suppliers Info
+                </TableRowHead>
+                <TableRowHead sx={{ textWrap: 'nowrap' }}>
+                  Address
+                </TableRowHead>
+                <TableRowHead sx={{ textWrap: 'nowrap' }}>
+                  Company
+                </TableRowHead>
+                <TableRowHead sx={{ textWrap: 'nowrap' }}>
+                  Delivery date
+                </TableRowHead>
+                <TableRowHead sx={{ textWrap: 'nowrap' }}>
+                  Amount
+                </TableRowHead>
+                <TableRowHead sx={{ textWrap: 'nowrap' }}>
+                  Status
+                </TableRowHead>
+                <TableRowHead sx={{ textWrap: 'nowrap' }}>
+                  Action
+                </TableRowHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Paper>
+            </TableHead>
+            <TableBody>
+              {suppliers.map((row, i) => (
+                <TableRow key={i}>
+                  <TableRowCell sx={{ textWrap: 'nowrap' }}>
+                    {row.name}
+                  </TableRowCell>
+                  <TableRowCell sx={{ textWrap: 'nowrap' }}>
+                    {row.address}
+                  </TableRowCell>
+                  <TableRowCell sx={{ textWrap: 'nowrap' }}>
+                    {row.suppliers}
+                  </TableRowCell>
+                  <TableRowCell sx={{ textWrap: 'nowrap' }}>
+                    {row.date}
+                  </TableRowCell>
+                  <TableRowCell sx={{ textWrap: 'nowrap' }}>
+                    {row.amount}
+                  </TableRowCell>
+                  <TableRowCell>
+                    <Chip
+                      label={row.status}
+                      sx={{
+                        height: '25px',
+                        fontSize: { mobile: '12px', tablet: '14px' },
+                        fontWeight: 400,
+                        lineHeight: { mobile: '14px', tablet: '16px' },
+                        backgroundColor: row.status === 'Active' ?
+                          'accent.light' : 'accent2.light',
+                        color: row.status === 'Active' ?
+                          'accent.main' : 'accent2.main'
+                      }}
+                    />
+                  </TableRowCell>
+                  <TableRowCell>
+                    <Button
+                      sx={{
+                        gap: '4px',
+                        height: '34px',
+                        width: '82px',
+                        fontSize: { mobile: '14px', tablet: '16px' },
+                        fontWeight: 400,
+                        lineHeight: { mobile: '16px', tablet: '18px' },
+                        textTransform: 'capitalize',
+                        color: 'acton.text.edit',
+                        borderRadius: '30px',
+                        borderWidth: '1px',
+                        borderStyle: 'solid',
+                        borderColor: 'acton.bg.edit',
+                        '&:hover': {
+                          color: 'accent.dark',
+                          backgroundColor: 'accent.light'
+                        }
+                      }}
+                      onClick={() => handleEdit(row.id)}
+                    >
+                      <Icon
+                        icon="majesticons:edit-pen-2-line"
+                        width="14"
+                        height="14"
+                      />
+                      Edit
+                    </Button>
+                  </TableRowCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+
+      <Popup open={openPopup} onClose={handleModal} title={'Edit supplier'}>
+        <SupplierForm suppItem={supplierItem} onClose={handleModal} />
+      </Popup>
+    </>
   )
 }
 
